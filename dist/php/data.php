76bc69c0a5396ddbfa2fd01028192ed64b29792c
Martuza <martuza.shimul@gmail.com>
2020-03-06 12:32:43 +0600

    quote engine added

--- dist/php/data.php
+++ dist/php/data.php
@@ -12,24 +12,24 @@ if(empty($_POST['first-name'])  		||
 	return false;
    }
 
-   // File upload functionality
-$new_path = "No files Uploaded";
-$counter = 0;
-if(!empty($_FILES['uploaded_file']))
-  {
-    $path = "uploads/";
-    $path = $path . basename( $_FILES['uploaded_file']['name']);
+//    // File upload functionality
+// $new_path = "No files Uploaded";
+// $counter = 0;
+// if(!empty($_FILES['uploaded_file']))
+//   {
+//     $path = "uploads/";
+//     $path = $path . basename( $_FILES['uploaded_file']['name']);
 
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-    //   $new_path = "<a href=" .$path. ">Check here</a>";
-    //   $new_path = "<a href=" echo $path ">Check here</a>";
-      $new_path = "https://handymanco.co.uk/dist/php/".$path;
-    //   $counter = 1;
+//     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+//     //   $new_path = "<a href=" .$path. ">Check here</a>";
+//     //   $new_path = "<a href=" echo $path ">Check here</a>";
+//       $new_path = "https://handymanco.co.uk/dist/php/".$path;
+//     //   $counter = 1;
   
-    } else{
-        echo "There was an error uploading the file, please try again!";
-    }
-  }
+//     } else{
+//         echo "There was an error uploading the file, please try again!";
+//     }
+//   }
 
 
 
@@ -37,10 +37,10 @@ if(!empty($_FILES['uploaded_file']))
 $service_type = $_POST['first'];
 // $second = $_POST['check'];
 // $type = implode(', ', $second); //vehicle type
-$service_name = $_POST['second'];
+$duration = $_POST['second'];
 //$area = $_POST['type']; // in which area
 $urgency = $_POST['third']; //timeline
-$materials = $_POST['fourth']; //material purchase
+//$materials = $_POST['fourth']; //material purchase
 
 
 // if(!empty($second)) {
@@ -54,7 +54,7 @@ $materials = $_POST['fourth']; //material purchase
 $first_name = $_POST['first-name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$postcode = $_POST['postcode'];
+$company = $_POST['postcode'];
 $message = $_POST['message'];
 
 	
@@ -63,13 +63,13 @@ $message = $_POST['message'];
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Quote Engine:  $first_name";
 
-$email_body = "You have received a new message from your Handyman website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $first_name\n Postcode: $postcode \nPhone: $phone\nEmail: $email\n Message: $message
+$email_body = "You have received a new message from your eMarketing London website Quote Engine .\n\n"."Here are the details:\n\ Name: $first_name\n Company Name: $company \nPhone: $phone\nEmail: $email\n Message: $message
 
-\n\n\n Here is the Quote Request: \n Place to service: $service_type\n Service Name: $service_name\n Urgency: $urgency\n Materials: $materials\n Uploaded File: " .$new_path;
+\n\n\n Here is the Quote Request: \n Service For: $service_type\n Duration: $duration\n Urgency: $urgency\n  ";
 
 // Weekly Price range: $price_range\n Area: $area \n How urgent : $urgency \n  For how long: $timeline 
 
-$headers = "From: noreply@handymanco.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@eMarketing-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
